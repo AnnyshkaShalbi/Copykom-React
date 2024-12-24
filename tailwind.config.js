7c4058fb9fef9a3e948a827c2a9e6cb2fb9c9bcc
Annyshka <104879302+AnnyshkaShalbi@users.noreply.github.com>
2024-12-24 11:57:19 +0300

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,17 @@
-/** @type {import('tailwindcss').Config} */
+
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        'primary': '#1E3A8F', 
+        background: "var(--background)",
+        foreground: "var(--foreground)",
       },
     },
   },
   plugins: [],
-}
+} 
