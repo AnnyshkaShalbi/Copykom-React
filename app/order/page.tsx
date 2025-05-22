4d788c562b8961575ff9ae0591e547aae5dad97a
Annyshka <104879302+AnnyshkaShalbi@users.noreply.github.com>
2025-05-22 19:04:05 +0300

    fix problem

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -1,13 +1,6 @@
-'use client'
 
 import type { Metadata } from "next";
-import { useOrder } from '@/app/context/OrderContext';
-
-import Title from "@/app/ui/common/title";
-import Steps from '../ui/order/steps'
-import StepOne from '../ui/order/stepOne/stepOne'
-import StepTwo from '../ui/order/stepTwo/stepTwo'
-import StepThree from '../ui/order/stepThree/stepThree';
+import Order from '@/app/ui/order/order'
 
 export const metadata: Metadata = {
   title: 'Твёрдый переплёт дипломов в Москве | Копиком',
@@ -19,18 +12,5 @@ export const metadata: Metadata = {
 };
 
 export default function Page() {
-  const { currentStep } = useOrder();
-  const subtitle = "Твёрдый перёплет сохранит документы в идеальном состоянии, повысит презентабельность их внешнего вида. Дипломы и диссертации будут защищены от посторонних воздействий и качественно скреплены."
-
-  return(
-    <div className="wrapper flex flex-col justify-between px-5">
-      <Title 
-        title="твёрдый переплёт дипломов"
-        subtitle={currentStep === 1 && subtitle} />
-      <Steps active={currentStep ?? 1} />
-      { currentStep === 1 && <StepOne /> }
-      { currentStep === 2 && <StepTwo /> }
-      { currentStep === 3 && <StepThree /> }
-    </div>
-  );
+  return <Order /> 
 }
