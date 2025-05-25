ff21e1b41dcac7f8cc0c86c55ebaf690bca6b35b
Annyshka <104879302+AnnyshkaShalbi@users.noreply.github.com>
2025-05-25 14:22:25 +0300

    add jsonLd for seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,8 +52,67 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const jsonLd = [
+    {
+      "@context": "https://schema.org",
+      "@type": "LocalBusiness",
+      "name": "Копиком на Авиамоторной",
+      "address": {
+        "@type": "PostalAddress",
+        "streetAddress": "ул. Красноказарменная, 15к1",
+        "addressLocality": "Москва",
+        "postalCode": "111250"
+      },
+      "telephone": "+ 7 (915) 431-06-66"
+    },
+    {
+      "@context": "https://schema.org",
+      "@type": "LocalBusiness",
+      "name": "Копиком на Аэропорте",
+      "address": {
+        "@type": "PostalAddress",
+        "streetAddress": "Ленинградский проспект, 66",
+        "addressLocality": "Москва",
+        "postalCode": "125315"
+      },
+      "telephone": "+ 7 (915) 431-06-66"
+    },
+    {
+      "@context": "https://schema.org",
+      "@type": "LocalBusiness",
+      "name": "Копиком на Бауманской",
+      "address": {
+        "@type": "PostalAddress",
+        "streetAddress": "ул. Ладожская, 10",
+        "addressLocality": "Москва",
+        "postalCode": "105066"
+      },
+      "telephone": "+ 7 (915) 431-06-66"
+    },
+    {
+      "@context": "https://schema.org",
+      "@type": "LocalBusiness",
+      "name": "Копиком на Октябрьской",
+      "address": {
+        "@type": "PostalAddress",
+        "streetAddress": "Ленинский проспект, 4с1А",
+        "addressLocality": "Москва",
+        "postalCode": "119049"
+      },
+      "telephone": "+ 7 (915) 431-06-66"
+    }
+  ]
+
   return (
-    <html lang="en">
+    <html lang="ru">
+      <head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+          key="organization-jsonld"
+        />
+      </head>
       <body className={`${noto_sans.className} antialiased flex flex-col min-h-screen`}>
         <Header />
         <main className="flex-grow pt-20 md:pt-40 mb-8 md:mb-11">
